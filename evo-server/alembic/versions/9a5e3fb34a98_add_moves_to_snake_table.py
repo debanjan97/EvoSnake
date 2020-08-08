64a47258e4bf1d77e140674c8f91c23f37a30b46
Debanjan Ganguly <debanjan.ganguly.98@gmail.com>
2020-08-08 18:21:35 +0530

    adds alembic skeleton

---
+++ evo-server/alembic/versions/9a5e3fb34a98_add_moves_to_snake_table.py
@@ -0,0 +1,24 @@
+"""add moves to snake table
+
+Revision ID: 9a5e3fb34a98
+Revises: 
+Create Date: 2020-08-08 12:44:48.177938
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9a5e3fb34a98'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
